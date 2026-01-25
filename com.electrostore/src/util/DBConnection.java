b152c252f20e28a292e457e1d49b5ae5ef96500e
md woalinur <mdwoalinur@gmail.com>
2026-01-25 11:03:56 +0600

    git and github test

---
+++ com.electrostore/src/util/DBConnection.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author HP
+ */
+public class DBConnection {
+    public static void main(String[] args) {
+        System.out.println("git and github repo test");
+    }
+}
